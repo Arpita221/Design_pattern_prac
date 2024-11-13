e55cee77d717a501b528cb02e3ae59d6c148096e
Arpita221 <arpitamajumder997@gmail.com>
2024-11-13 22:54:00 +0600

    this is chain

--- src/main/java/Chain/Logger.java
+++ src/main/java/Chain/Logger.java
@@ -2,4 +2,24 @@ package Chain;
 
 abstract class Logger {
 
+
+    protected int level;
+    protected Logger nextLogger;
+
+    // Set the next logger in the chain
+    public void setNextLogger(Logger nextLogger) {
+        this.nextLogger = nextLogger;
+    }
+
+    public void logMessage(int level, String message) {
+        if (this.level <= level) {
+            write(message);
+        }
+        if (nextLogger != null) {
+            nextLogger.logMessage(level, message);
+        }
+    }
+
+    // Abstract method to be implemented by concrete loggers
+    protected abstract void write(String message);
 }
