74ff44e2930e66fb71095ba9b83d04ba5fd7b842
Arpita221 <arpitamajumder997@gmail.com>
2024-11-13 23:50:37 +0600

    this is chained

---
+++ src/main/java/Chain/DebugLogger.java
@@ -0,0 +1,14 @@
+package Chain;
+
+public class DebugLogger extends Logger
+{
+    public DebugLogger(int level) {
+        this.level = level;
+    }
+
+
+    @Override
+    protected void write(String message) {
+        System.out.println("DEBUG: " + message);
+    }
+}
