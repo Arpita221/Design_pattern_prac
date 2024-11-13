142d17c5197d52cb2112a6a4b47b956edbbb2354
Arpita221 <arpitamajumder997@gmail.com>
2024-11-13 18:23:01 +0600

    git init

--- src/main/java/Singleton/SingletonPrac.java
+++ src/main/java/Singleton/SingletonPrac.java
@@ -1,4 +1,12 @@
 package Singleton;
 
 public class SingletonPrac {
+    private final String  data ;
+    private static volatile SingletonPrac instance;
+    private Singleton(String data){
+        this.data=data;
+
+    }
+
+
 }
